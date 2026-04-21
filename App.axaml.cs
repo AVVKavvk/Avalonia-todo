92cec029e994a96bbe5362845978b04d9eafb3fe
Vipin Kumawat <kumawatvipin066@gmail.com>
2026-04-21 14:49:03 +0530

    setup

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,11 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using TodoApp.ViewModels;
+using TodoApp.Views;
 
 namespace TodoApp;
 
@@ -15,7 +20,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow { DataContext = new MainWindowViewModel() };
         }
 
         base.OnFrameworkInitializationCompleted();
